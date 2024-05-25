3f08c196ba86d9fbb34d2a2de0bde43323d0e327
kuba <kuba.pro010@gmail.com>
2024-05-25 12:08:27 +0200

    add files

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .interface import GroupInterface
+from .comfort import Groups, GroupSequencer
+from .af import AF_Bands, AlternativeFrequencyEntry
+from .generator import GroupGenerator, Group
+librds_version = 1.2
+pyrds_version = librds_version
