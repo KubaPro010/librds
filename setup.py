2f20b8d0a710e7b875048c3f713523154070e6bd
kuba <kuba.pro010@gmail.com>
2024-05-27 19:46:09 +0200

    rightttt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
         name="librds", 
-        version="1.3.8",
+        version="1.381",
         author="kuba201",
         description='RDS Group Generator',
         long_description=readme,
